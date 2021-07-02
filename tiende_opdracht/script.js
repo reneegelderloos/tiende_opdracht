03b1f0980a8add2d1468e91d2a4e3e3fba5c96f2
reneegelderloos <84631679+reneegelderloos@users.noreply.github.com>
2021-07-02 22:44:05 +0200

    Add files via upload

---
+++ tiende_opdracht/script.js
@@ -0,0 +1,21 @@
+const age = function (number) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const age2 = function () {
+    console.log(age(number));
+    if ()
+}
+
+
+/*
+this second function uses (calls) the first function, giving it the age, to check if we get an adult age
+if it's an adult the second function should return "Hello there"
+if it's not an adult it should return "Hey kiddo"
+use console.log to display the return value of the second function
+experiment so you're sure your function works
+*/
